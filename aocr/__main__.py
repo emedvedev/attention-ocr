0018ccddac2e001c31623ee4b6f3f0c6a0d038c9
Richard Brooks <richardbrks@RichardFlock.local>
2017-12-18 08:41:06 -0500

    correct parse_args usage

--- aocr/__main__.py
+++ aocr/__main__.py
@@ -247,7 +247,7 @@ def process_args(args, defaults):
                               (defaults.CLIP_GRADIENTS)))
     parser.set_defaults(clip_gradients=defaults.CLIP_GRADIENTS)
 
-    parameters, _ = parser.parse_args(args)
+    parameters = parser.parse_args()
     return parameters
 
 
