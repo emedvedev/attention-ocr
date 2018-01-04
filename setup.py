0b0dc29a630c98c63f79e0651edb3f59481c470a
Edward Medvedev <edward.medvedev@gmail.com>
2018-01-03 21:56:07 -0800

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = ['distance', 'numpy', 'six', 'pillow']
-VERSION = '0.3.0'
+VERSION = '0.5.0'
 try:
     import pypandoc
     README = pypandoc.convert('README.md', 'rst')
