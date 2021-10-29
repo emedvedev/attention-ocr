49503aaf873b44c0262adb781f273c2f21ee6739
stickler-ci <support@stickler-ci.com>
2021-10-29 14:40:23 +0000

    Fixing style errors.

--- aocr/util/export.py
+++ aocr/util/export.py
@@ -30,7 +30,7 @@ class Exporter(object):
                 },
                 clear_devices=True)
             builder.save()
-            
+
             logging.info("Exported SavedModel into %s", path)
 
         elif model_format == "frozengraph":
